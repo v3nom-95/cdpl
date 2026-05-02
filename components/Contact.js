4edc3e5d9a23ba0b2143c894d706438714d54ab0
v3nom-95 <user@example.com>
2026-05-02 08:44:37 +0530

    contact page - twitter removal

--- components/Contact.js
+++ components/Contact.js
@@ -72,16 +72,6 @@ export default function Contact() {
             ),
         },
         {
-            id: 'x',
-            label: 'X (Twitter)',
-            value: '@chakravyuha',
-            href: 'https://x.com/cdpl',
-            external: true,
-            icon: (
-                <svg width="22" height="22" viewBox="0 0 24 24" fill="currentColor"><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/></svg>
-            ),
-        },
-        {
             id: 'location',
             label: 'HQ_LOCATION',
             value: 'Madhapur, Hyderabad, India',
